b3334bc21e8988d3becaeb11fb18b6c47ba85e86
kendo5731 <aur.baumann@gmail.com>
2015-12-07 23:21:56 +0100

    chore(build): edit build.sh to automatically copy generated binary to the rasp

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 # Build for my raspberry
+echo "Compiling ..."
 GOARCH=arm GOOS=linux go build
+echo "Copying new binary to raspberry ..."
+scp ./http_files_client pi@raspberry:/usr/local/bin/httpfilesclient
+
