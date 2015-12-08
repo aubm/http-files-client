97bdbe6e7dbaa33e7ecb8dad3002c4ee8b10695d
kendo5731 <aur.baumann@gmail.com>
2015-12-08 21:33:49 +0100

    feat(main): download files 5 by 5 instead of downloading all files in the same time

--- main.go
+++ main.go
@@ -10,24 +10,51 @@ func main() {
 	app.SetGlobals()
 	defer loggers.InitLoggers().Close()
 
-	files, err := remote.GetFilesList()
+	filesList, err := remote.GetFilesList()
 	if err != nil {
 		loggers.Error.Fatalf("failed to fetch new available files, %v", err)
 	}
 
 	ch := make(chan remote.Status)
-	for _, file := range files {
-		go remote.DownloadAndDelete(file, ch)
-	}
-	for range files {
-		status := <-ch
-		if !status.DownloadSuccess {
-			loggers.Error.Printf("download failed [%v], elapsed time [%v], %v", status.File, status.Elapsed, status.Error)
-		} else {
-			loggers.Info.Printf("download success [%v], elapsed time [%v]", status.File, status.Elapsed)
-			if !status.RemoveSuccess {
-				loggers.Error.Printf("remove failed [%v], %v", status.File, status.Error)
+	for _, files := range chunkFiles(filesList) {
+		for _, file := range files {
+			go remote.DownloadAndDelete(file, ch)
+		}
+		for range files {
+			status := <-ch
+			if !status.DownloadSuccess {
+				loggers.Error.Printf("download failed [%v], elapsed time [%v], %v", status.File, status.Elapsed, status.Error)
+			} else {
+				loggers.Info.Printf("download success [%v], elapsed time [%v]", status.File, status.Elapsed)
+				if !status.RemoveSuccess {
+					loggers.Error.Printf("remove failed [%v], %v", status.File, status.Error)
+				}
 			}
+		}
+	}
+}
+
+func chunkFiles(files []string) [][]string {
+	nbFiles := len(files)
+	chunksSize := 5
+	start := 0
+	end := chunksSize
+	var chunks [][]string
+	for {
+		if start <= nbFiles {
+			if end > nbFiles {
+				end = nbFiles
+			}
+			newchunk := files[start:end]
+			if chunks == nil {
+				chunks = [][]string{newchunk}
+			} else {
+				chunks = append(chunks, newchunk)
+			}
+			start += chunksSize
+			end += chunksSize
+		} else {
+			return chunks
 		}
 	}
 }
